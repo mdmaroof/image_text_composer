f5039f8d3be19809bc9b20e61bf11c864048380a
Mohd Maroof <mohdmaroof@VAHN-FE1.local>
2025-08-12 19:25:03 +0530

    folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 export default function Home() {
   return (
     <div className="relative flex flex-col gap-4 px-4 py-4 w-full h-dvh">
-      <main className="flex flex-1 gap-4">
-        <div className="flex-1 border rounded-md w-full h-full">Canvas</div>
+      <div className="flex flex-1 gap-4">
+        <section id="canvas" className="flex-1 border rounded-md w-full h-full">
+          Canvas
+        </section>
         <section id="layers" className="border rounded-md w-80 h-full">
           Layers
         </section>
-      </main>
-
-      <section id="text_tools" className="border rounded-md h-72"></section>
+      </div>
+      <section id="selector" className="border rounded-md h-72"></section>
     </div>
   );
 }
